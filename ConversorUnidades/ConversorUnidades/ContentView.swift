3ae525d9032835ac9582359eadc381c56a57ecac
Ivan Pedron <ivan.pedron@ikasle.egibide.org>
2023-01-10 19:22:09 +0100

    Comit 1

--- ConversorUnidades/ConversorUnidades/ContentView.swift
+++ ConversorUnidades/ConversorUnidades/ContentView.swift
@@ -7,11 +7,39 @@
 
 import SwiftUI
 
+enum medida: String, CaseIterable{
+    case centimetros
+    case metros
+    case kilometros
+
+}
+
 struct ContentView: View {
+    
+    @State private var medidaSeleccionada = medida.metros
+    @State private var cantidad = 50.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack{
+            Divider()
+            
+            Picker("Medida", selection:$medidaSeleccionada){
+                ForEach(medida.allCases,id:\.self){med in
+                    Text(med.rawValue.capitalized)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+        Slider(
+            value: $cantidad,
+            in: 0...100,
+            step: 0.25,
+            onEditingChanged: { editing in
+                isEditing = editing
+            },
+            minimumValueLabel: Text("0"),
+            maximumValueLabel: Text("100")
+        )    }
 }
 
 struct ContentView_Previews: PreviewProvider {
